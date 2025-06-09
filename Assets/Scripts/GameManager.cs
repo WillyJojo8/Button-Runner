d3e696cbdbc82d574257049f4d8217908e75d4aa
WillyJojo8 <lacasitos1333@gmail.com>
2025-06-09 19:39:08 +0200

    Primer commit real - Juego Base

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,30 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public enum GameState { Ready, Playing};
+public enum GameState { Ready, Playing, Ended };
 
 public class GameManager : MonoBehaviour
 {
     public GameState gameState = GameState.Ready;
     public RawImage background, platform;
     public float parallaxSpeed = 0.02f;
-    public GameObject uiReady;
+    public GameObject uiReady, uiScore;
 
     void Update()
     {
         bool action = Input.GetKeyDown("space") || Input.GetMouseButtonDown(0);
 
         HandleJump(action);
+        HandleCollisions();
         UpdateParallax();
         UpdateGameState(action);
+        HandleExit();
     }
 
     void HandleJump(bool action)
     {
-        if (gameState == GameState.Playing && action)
+        if (gameState == GameState.Playing && action && PlayerManager.Instance.grounded)
         {
             PlayerManager.Instance.SetAnimation("PlayerJump");
+        }
+    }
+
+    void HandleCollisions()
+    {
+        if (gameState == GameState.Playing && PlayerManager.Instance.enemyCollision)
+        {
+            gameState = GameState.Ended;
+            PlayerManager.Instance.SetAnimation("PlayerDie");
+            SpawnManager.Instance.StopSpawn();
+            SpeedManager.Instance.ResetSpeed();
         }
     }
 
@@ -34,9 +48,18 @@ public class GameManager : MonoBehaviour
         if (gameState == GameState.Ready && action)
         {
             gameState = GameState.Playing;
+
             uiReady.SetActive(false);
+            uiScore.SetActive(true);
 
             PlayerManager.Instance.SetAnimation("PlayerRun");
+            SpawnManager.Instance.StartSpawn();
+            SpeedManager.Instance.StartSpeedIncrease();
+        }
+
+        else if (gameState == GameState.Ended && action)
+        {
+            HandleRestart();
         }
     }
 
@@ -48,5 +71,15 @@ public class GameManager : MonoBehaviour
             background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
             platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
         }
+    }
+
+    void HandleRestart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    void HandleExit() 
+    {
+        if (Input.GetKeyDown("escape")) Application.Quit();
     }
 }
