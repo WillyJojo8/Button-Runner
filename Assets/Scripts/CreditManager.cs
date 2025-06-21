809c55c4ab4688ed38fc1ce47fb319018de60518
pSenciales <124668501+pSenciales@users.noreply.github.com>
2025-06-21 14:41:32 +0200

    feat: Credit scene

---
+++ Assets/Scripts/CreditManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CreditManager : MonoBehaviour
+{
+
+
+    public Animation creditAnimation;
+    public AudioSource musicSource;
+    public string nextSceneName = "Main";
+
+    void Update()
+    {
+        // Aceleración (ya lo tienes)
+        bool accelerating = Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0);
+        float speed = accelerating ? 4f : 1f;
+
+        if (creditAnimation != null && creditAnimation.clip != null)
+        {
+            var clipName = creditAnimation.clip.name;
+            creditAnimation[clipName].speed = speed;
+
+            if (!creditAnimation.IsPlaying(clipName))
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+
+        if (musicSource != null)
+            musicSource.pitch = accelerating ? 2f : 1f;
+    }
+
+   
+}
